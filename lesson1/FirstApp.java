302c1c130b7cf927f1c36497f3c88594ff2fc18e
Jimmy <jimmy.hokkins@yandex.ru>
2021-01-24 12:36:41 +0300

    updated FirstApp.java

--- lesson1/FirstApp.java
+++ lesson1/FirstApp.java
@@ -2,6 +2,21 @@ package lesson1;
 
 public class FirstApp {
     public static void main(String[] args) {
+        byte bt = 50;
+        short shrt = 500;
+        int intgr = 308779;
+        long lng = 2345654321345L;
+        float flt = 12.4f;
+        double dbl = 23.999;
+        char chr = 'A';
+        boolean bln = true;
+    }
 
+    public static float calcFloat(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+
+    public static boolean checkSum(int a, int b) {
+        return ((a + b) >= 10 && (a + b) <= 20);
     }
 }
