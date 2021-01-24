09bc69c8fff3ec0a089f8f15476acb910ec29745
Jimmy <jimmy.hokkins@yandex.ru>
2021-01-24 12:51:09 +0300

    updated FirstApp.java again

--- lesson1/FirstApp.java
+++ lesson1/FirstApp.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class FirstApp {
     public static void main(String[] args) {
         byte bt = 50;
@@ -10,6 +12,16 @@ public class FirstApp {
         double dbl = 23.999;
         char chr = 'A';
         boolean bln = true;
+
+        int year;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter the year: ");
+            year = sc.nextInt();
+            if (checkYear(year)) System.out.println("The year is leap.");
+            else System.out.println("The year is not leap.");
+        } while(year != 0);
     }
 
     public static float calcFloat(float a, float b, float c, float d) {
@@ -18,5 +30,11 @@ public class FirstApp {
 
     public static boolean checkSum(int a, int b) {
         return ((a + b) >= 10 && (a + b) <= 20);
+    }
+
+    public static boolean checkYear(long year) {
+        if (year <= 0 || year >= Long.MAX_VALUE) return false;
+        if (year % 400 == 0) return true;
+        return (year % 4 == 0) && (year % 100 != 0);
     }
 }
